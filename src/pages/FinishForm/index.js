72449b817b1df91ced53371dd921c1d7e385a2da
RafaSilvaDev <rafabdasilva12@gmail.com>
2021-10-28 09:15:24 -0300

    Making final screen

---
+++ src/pages/FinishForm/index.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { View, TouchableOpacity } from "react-native";
+import firebase from "../../config/firebaseconfig";
+import styles from "./styles";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+export default function FinishForm({ navigation }) {
+    return(
+        <View style={styles.container}>
+            
+        </View>
+    );
+}
